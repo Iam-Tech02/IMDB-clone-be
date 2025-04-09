e56139a909df621be0bbed583032a427df61e0a5
I am DEV <157353142+Iam-Tech02@users.noreply.github.com>
2025-04-09 20:40:53 +0530

    Update db.js

--- imdb-backend/config/db.js
+++ imdb-backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-   const uri =  "mongodb+srv://jmanpreet1000:XaBj8CuYb4gS51Rs@manpreet.8dpj7fr.mongodb.net/"
+   const uri =  "mongodb+srv://jmanpreet1000:XaBj8CuYb4gS51Rs@manpreet.8dpj7fr.mongodb.net/imdb?retryWrites=true&w=majority"
   try {
     await mongoose.connect(uri);
     console.log('MongoDB connected successfully');
